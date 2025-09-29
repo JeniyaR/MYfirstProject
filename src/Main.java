ffa5420f7d9ade3817da17e23e87a5938d6f53f3
JeniyaR <evrose7@gmail.com>
2025-09-30 00:11:02 +0300

    первый

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(6+1);
+        System.out.println("Hello, Jenka" + "  and all friends");
+        System.out.println("Hello, Jenka" + 444);
+        System.out.println("Hello, 3i/atlas 789");
+        System.out.println(6*0);
+
+    }
+}
