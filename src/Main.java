e02674b4747273af3f91c501faa2bb2f5b47f880
JeniyaR <evrose7@gmail.com>
2025-10-05 22:36:21 +0300

    Сделал задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //2.1 к 1-ому видео
         System.out.println(6+1);
         System.out.println("Hello, Jenka" + "  and all friends");
         System.out.println("Hello, Jenka" + 444);
@@ -8,6 +9,48 @@ public class Main {
         System.out.println(6*0);
         System.out.println(6*5);
 
+// 2.1 задание  (my name)
+        System.out.println("My name is Java!");
+
+//2.1 задание   (I like programming)
+        System.out.println("I like programming!");
+        System.out.println("This is my homework");
+        System.out.println("5+1");
+        System.out.println("100");
+
+// 2.1 задание   (math operations)
+        System.out.println("Math operations");
+        System.out.println("900 - 10000");
+        System.out.println("5 + 100");
+        System.out.println("100" + " + 100");
+
+//2.1 задание    (Java это интересно)
+        System.out.println("Изучать Java — это интересно!");
+        System.out.println("Не бойся делать ошибки.");
+        System.out.println("Каждый день ты становишься лучше!");
+
+// 2.1 задание     (список покупок)
+        System.out.println("Список покупок:");
+        System.out.println("1." + " Хлеб");
+        //System.out.println("2." + " Молоко");
+        /*System.out.println("3." + " Яблоки");*/
+        System.out.println("4." + "  Шоколад");
+
+// 2.1 задание   (однострочные комментарии)
+        int a = 5;//число a равно 5
+        int b = 7;// число b равно 7
+        int sum = a + b;// число sum равно сумме a+b
+        System.out.println(sum);//вывод числа sum
+
+// 2.1 задание  (многострочный комментарий)
+        /*System.out.println("Привет!");
+        System.out.println("Это тестовая программа.");*/
+        System.out.println("Удачи!");
+
+// 2.1 задание   (многострочный комментарий)
+        System.out.println("Имя: Алексей");
+        /*System.out.println("Возраст: 25");
+        System.out.println("Город: Москва");*/
     }
 }
 
